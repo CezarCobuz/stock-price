91e0801980f2ed0646cf4f3e5a87742d8fbdb481
CezarCobuz <cobuzcezar@gmail.com>
2020-05-10 15:14:57 +0300

    Updates

---
+++ precache-manifest.d9895ad73c3931c19c7fec8b6df16e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464d6c5fea13b92bc3ce759ba1bcb906",
+    "url": "/stock-price/index.html"
+  },
+  {
+    "revision": "4cf1b26c41a180c6f12e",
+    "url": "/stock-price/static/css/main.1b965f31.chunk.css"
+  },
+  {
+    "revision": "882e5e1ec513dc7a65a8",
+    "url": "/stock-price/static/js/2.a735792a.chunk.js"
+  },
+  {
+    "revision": "0229f2113b78559ca1daeb1dd663a5a5",
+    "url": "/stock-price/static/js/2.a735792a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf1b26c41a180c6f12e",
+    "url": "/stock-price/static/js/main.5fc478a0.chunk.js"
+  },
+  {
+    "revision": "8cfa91974bb3319a6dc5",
+    "url": "/stock-price/static/js/runtime-main.da7f27f9.js"
+  }
+]);
